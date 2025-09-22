5829cfe5fe2ccb1c881f8e753c108cb7764666e9
ben <x_z_gfree@163.com>
2025-09-22 14:19:03 +0800

    feat: show iOS build task

--- src/cli/hugo-aiv/buildHugoAivApp.ts
+++ src/cli/hugo-aiv/buildHugoAivApp.ts
@@ -32,9 +32,12 @@ const applications = ["android", "iOS"]; // 系统
 const envs = ["alpha", "production"]; // 环境配置
 const branch = ["alpha", "main"]; // 代码分支
 
-const pipelineOptions = cartesian4(androidPackages, applications, envs, branch)
-  .filter((item) => !item.includes("iOS"))
-  .map((item) => item.join("-"));
+const pipelineOptions = cartesian4(
+  androidPackages,
+  applications,
+  envs,
+  branch
+).map((item) => item.join("-"));
 
 export async function buildHugoAivApp() {
   try {
